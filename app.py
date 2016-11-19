19f5603b720e40112e299e0ebc66a25f90053a75
Tim <tim.bathgate@gmail.com>
2016-11-20 12:53:18 +1300

    Add movies

--- app.py
+++ app.py
@@ -14,7 +14,23 @@ Example:
 from movie import Movie
 from fresh_tomatoes import open_movies_page
 
-movies = [Movie('x', 'y', 'z')]
+movies = [
+    Movie(
+        'Boudu Saved From Drowning',
+        'https://upload.wikimedia.org/wikipedia/en/c/c4/Boudu_sauv%C3%A9_des_eaux.jpg',
+        'https://www.youtube.com/watch?v=PYkB5W4lYsQ'
+    ),
+    Movie(
+        'A Hard Day\'s Night',
+        'https://s-media-cache-ak0.pinimg.com/236x/dc/1e/cd/dc1ecd50b2d0e8db32c95e0cda98bd09.jpg',
+        'https://www.youtube.com/watch?v=q0eJEX5c1sM'
+    ),
+    Movie(
+        'The Exploding Girl',
+        'https://upload.wikimedia.org/wikipedia/en/c/c0/The_exploding_girl.jpg',
+        'https://www.youtube.com/watch?v=vUB3NNbVWnA'
+    )
+]
 
 if (__name__ == "__main__"):
     open_movies_page(movies)
